c9b6debc9abeb80d01eb368d6e276231111afdb2
Evgeniy Kvasov <E_kvasov@inbox.ru>
2023-08-27 11:18:21 +0500

    init: start and finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const module = Math.abs(n);
+  let reverseNumber = '';
+  for (i = String(module).length - 1; i >= 0; i--) {
+    reverseNumber += String(module)[i];
+  }
+  return Number(reverseNumber);
 }
